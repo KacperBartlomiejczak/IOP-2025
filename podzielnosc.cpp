0aadd16445ee0dc9c6f97eda498519b0aeb673d8
Kacper Bartlomiejczak <kacperbartlomiejczak@proton.me>
2025-12-11 13:57:57 +0100

    Dodano funkcje NWD i wyswietlanie wyniku

--- podzielnosc.cpp
+++ podzielnosc.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
 using namespace std;
 
+// Funkcja obliczająca NWD
+int nwd(int a, int b) {
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
 int main() {
     int a, b;
     cin >> a >> b;
-    cout << a << " " << b << endl;
+    // Zmiana w wypisywaniu - dodajemy NWD
+    cout << a << " " << b << " " << nwd(a, b) << endl;
     return 0;
 }
