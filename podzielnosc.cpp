f9e2c6abb4711d9a15e00e47541e3df04749c22d
Kacper Bartlomiejczak <kacperbartlomiejczak@proton.me>
2025-12-11 13:58:51 +0100

    Dodano walidacje liczb dodatnich

--- podzielnosc.cpp
+++ podzielnosc.cpp
@@ -4,6 +4,13 @@ using namespace std;
 int main() {
     int a, b;
     cin >> a >> b;
+    
+    // Walidacja
+    if (a <= 0 || b <= 0) {
+        cout << "Blad: Liczby musza byc dodatnie!" << endl;
+        return 0; // Kończymy program
+    }
+
     cout << a << " " << b << endl;
     return 0;
 }
